f97e9b6b48622db15c468231298bcba5daa8de02
Numerica98 <00041517@uca.edu.sv>
2019-05-14 11:51:49 -0600

    Clase NewWordActivity terminada, queda pendiente arreglar WordRoomDatabase y WordViewModel. Se llego hasta el paso 13 en total y a comenzar el 14

--- app/src/main/java/com/example/roomwordsample/NewWordActivity.kt
+++ app/src/main/java/com/example/roomwordsample/NewWordActivity.kt
@@ -1,7 +1,10 @@
 package com.example.roomwordsample
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +19,20 @@ class NewWordActivity : AppCompatActivity() {
         editWordView = findViewById(R.id.edit_word)
 
         val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editWordView.text)){
+                setResult(Activity.RESULT_CANCELED,replyIntent)
+            } else{
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
     }
 }
